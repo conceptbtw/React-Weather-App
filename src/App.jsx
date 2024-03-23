929415381cb8872c9e9f630a0b19ef5a04cee943
Illia Movchko <illia.movchko.concept@gmail.com>
2024-03-23 20:22:31 +0000

    Create Header, Main and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
